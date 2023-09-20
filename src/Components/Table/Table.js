7b52cbffc560043fee82399f97fb3feacc5d533f
Arij Zitouni <Arij.Zitouni@tn.ey.com>
2023-09-20 17:37:41 +0100

    final commit after last changes

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -12,16 +12,17 @@ import Tooltip from '@mui/material/Tooltip';
 import './Table.css';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { FcApproval, FcCancel } from "react-icons/fc";
-import Alert from '@mui/material/Alert';
-import { IconButton, Snackbar } from '@mui/material';
+import { FcApproval, FcCancel } from 'react-icons/fc';
+import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar'; // Use MUI Snackbar
+import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { format } from 'date-fns';
-import { GridLoader } from 'react-spinners';
-import { ApiUrls } from '../../config/ApiUrls';
+import { Alert } from '@mui/material';
 
 function CustomPaginationActionsTable() {
   const [openAlert, setOpenAlert] = useState(false);
+
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [stack, setStack] = useState([]);
@@ -31,7 +32,7 @@ function CustomPaginationActionsTable() {
 
   useEffect(() => {
     axios
-      .get(ApiUrls.FetchComponents)
+      .get(`https://zenml-frontend-challenge-backend.fly.dev/components`)
       .then((response) => {
         const stackData = response.data;
         setStack(stackData);
@@ -39,6 +40,7 @@ function CustomPaginationActionsTable() {
       })
       .catch((error) => {
         setError(error);
+        setOpenAlert(true); // Open the Snackbar when an error occurs
         setLoading(false);
       });
   }, [stackId]);
@@ -51,23 +53,6 @@ function CustomPaginationActionsTable() {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-  if (loading) {
-    return (
-      <div className="loader">
-        <GridLoader
-          color="#7c3679"
-          loading={loading}
-          size={30}
-          style={{
-            marginTop: '15vh',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            display: 'block',
-          }}
-        />
-      </div>
-    );
-  }
 
   const handleCloseAlert = (event, reason) => {
     if (reason === 'clickaway') {
@@ -75,113 +60,65 @@ function CustomPaginationActionsTable() {
     }
     setOpenAlert(false);
   };
-  if (error) {
+
+  if (loading) {
     return (
-      <div className="error">
-        <Snackbar
-          open={openAlert}
-          autoHideDuration={6000}
-          onClose={handleCloseAlert}
-          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        >
-          <Alert
-            severity="error"
-            action={
-              <IconButton
-                size="small"
-                aria-label="close"
-                color="inherit"
-                onClick={handleCloseAlert}
-              >
-                <CloseIcon fontSize="small" />
-              </IconButton>
-            }
-            sx={{ zIndex: 2000 }}
-          >
-            Error: {error.message}
-          </Alert>
-        </Snackbar>
+      <div className="loader">
+        <CircularProgress color="primary" />
       </div>
     );
   }
 
-  const startIndex = page * rowsPerPage;
-  const endIndex = startIndex + rowsPerPage;
-
   return (
     <div className="stacks-details">
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        m={2}
-      >
+      <Box display="flex" flexDirection="column" alignItems="center" m={2}>
         <Paper
           elevation={4}
-          sx={{ borderRadius: 10, boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}
+          sx={{
+            borderRadius: 10,
+            boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          }}
         >
           <TableContainer>
-            <Table
-              sx={{ minWidth: 500, paddingTop: '18px', marginBottom: "8px" }}
-              aria-label="custom pagination table"
-            >
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center" style={{ fontWeight: 'bold' }}>
-                    ID
-                  </TableCell>
-                  <TableCell align="center" style={{ fontWeight: 'bold' }}>
-                    Created
-                  </TableCell>
-                  <TableCell align="center" style={{ fontWeight: 'bold' }}>
-                    Name
-                  </TableCell>
-                  <TableCell align="center" style={{ fontWeight: 'bold' }}>
-                    Shared
-                  </TableCell>
-                  <TableCell align="center" style={{ fontWeight: 'bold' }}>
-                    Flavor
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {stack.slice(startIndex, endIndex).map((item) => (
-                  <TableRow key={item.id}>
-                    <TableCell component="th" scope="row">
-                      <Tooltip title={item.id} arrow>
-                        <span>{item.id.substring(0, 8)}...</span>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="center">
-                      {format(new Date(item.created), 'MM/dd/yyyy HH:mm:ss')}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="center">
-                      {item.name}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="center">
-                      {item.is_shared === true ? <FcApproval /> : <FcCancel />}
-                    </TableCell>
-                    <TableCell style={{ width: 160 }} align="center">
-                      {item.flavor}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+            {/* ... Rest of your table code ... */}
           </TableContainer>
         </Paper>
 
         <TablePagination
-          sx={{ borderRadius: 10, boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)' }}
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={stack.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
+          sx={{
+            borderRadius: 10,
+            boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          }}
+          // ... Rest of your pagination code ...
         />
       </Box>
+
+      {/* Snackbar for displaying errors */}
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        // Adjust the position of the Snackbar based on your design
+        sx={{ top: '50px' }}
+      >
+        <Alert
+          severity="error"
+          action={
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleCloseAlert}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          }
+          sx={{ zIndex: 2000 }}
+        >
+          Error: {error?.message || 'Unknown error'}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
